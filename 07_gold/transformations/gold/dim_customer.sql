30139a26f57e1607c6710381d56fa6125d6fd6a9
Skoga02 <julius.faltskoglund@gmail.com>
2026-05-22 09:29:23 +0000

    gold_layer

---
+++ 07_gold/transformations/gold/dim_customer.sql
@@ -0,0 +1,13 @@
+CREATE OR REFRESH MATERIALIZED VIEW supply_chain_live.gold.dim_customer
+COMMENT "Dim customer deduplicated - gold layer" AS
+SELECT
+  customer_id,
+  MAX_BY(customer_fname, order_date) AS first_name,
+  MAX_BY(customer_lname, order_date) AS last_name,
+  MAX_BY(customer_country, order_date) AS country
+FROM
+  supply_chain_live.silver.supply_chain_obt
+GROUP BY
+  customer_id
+ORDER BY
+  customer_id;
